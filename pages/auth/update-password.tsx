b6e4e45fd75878cfc64ec91144779ffe8e69889f
Yavar Babayev <yaverbabayev@gmail.com>
2025-07-08 05:23:40 +0200

    Update update-password.tsx

--- pages/auth/update-password.tsx
+++ pages/auth/update-password.tsx
@@ -1,7 +1,6 @@
-import AuthWrapper from "@/components/AuthWrapper";
-
 'use client'
 
+import AuthWrapper from "@/components/AuthWrapper";
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/lib/supabaseClient'
